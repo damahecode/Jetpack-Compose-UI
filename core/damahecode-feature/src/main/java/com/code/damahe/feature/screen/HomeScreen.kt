a3259d32203fb1334aacda637d17aeadca704f28
Damahe Code <damahecode@gmail.com>
2023-06-19 13:31:38 +0530

    minor changes

--- core/damahecode-feature/src/main/java/com/code/damahe/feature/screen/HomeScreen.kt
+++ core/damahecode-feature/src/main/java/com/code/damahe/feature/screen/HomeScreen.kt
@@ -1,16 +1,12 @@
 package com.code.damahe.feature.screen
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
-import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,6 +19,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
@@ -30,7 +28,6 @@ import com.code.damahe.res.R
 import com.code.damahe.res.config.AppActivityObject
 import com.code.damahe.res.icon.MyIcons
 import com.code.damahe.res.icon.DCodeIcon.ImageVectorIcon
-import com.code.damahe.res.config.PreferenceScreenNavigation.preferenceScreenNavRoute
 
 @OptIn(
     ExperimentalComposeUiApi::class,
@@ -66,21 +63,22 @@ fun HomeScreen(
             )
         },
     ) { padding ->
-        Row(
-            Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .consumeWindowInsets(padding)
-                .windowInsetsPadding(
-                    WindowInsets.safeDrawing.only(
-                        WindowInsetsSides.Horizontal,
-                    ),
-                ),
+        HomeScreenContent(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun HomeScreenContent(
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+
+    Box(modifier = modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.testTag("HOME_SCREEN_LIST")
         ) {
-
-
-
 
         }
     }
+
 }
