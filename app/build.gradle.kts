2fe93a4b647884c560119a9ad908668040fcd7fc
Damahe Code <damahecode@gmail.com>
2023-06-23 12:03:50 +0530

    minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.code.damahe.jetpack.compose.ui"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
