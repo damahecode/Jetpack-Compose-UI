c3e32b3443827c67765370f0453a8f7a7ff923eb
Damahe Code <damahecode@gmail.com>
2024-07-31 13:31:53 +0530

    updated list

--- core/ui/src/main/java/com/code/damahe/ui/utils/DemoDataProvider.kt
+++ core/ui/src/main/java/com/code/damahe/ui/utils/DemoDataProvider.kt
@@ -6,15 +6,27 @@ import com.code.damahe.res.R
 import com.code.damahe.res.icon.DCodeIcon
 import com.code.damahe.res.icon.DCodeIcon.ImageVectorIcon
 import com.code.damahe.res.icon.MyIcons
+import com.code.damahe.res.navigation.MainActivityNavigation.chatScreenNavRoute
+import com.code.damahe.res.navigation.MainActivityNavigation.emptyScreenNavRoute
+import com.code.damahe.res.navigation.MainActivityNavigation.locBiometricScreenNavRoute
+import com.code.damahe.res.navigation.MainActivityNavigation.loginSignupScreenNavRoute
+import com.code.damahe.res.navigation.MainActivityNavigation.onBoardingScreenNavRoute
+import com.code.damahe.res.navigation.MainActivityNavigation.profileScreenNavRoute
+import com.code.damahe.res.navigation.MainActivityNavigation.settingsScreenNavRoute
 
 object DemoDataProvider {
 
     fun demoUIListItems(context: Context) = listOf(
-        FeaturedList(context.getString(R.string.profile), ImageVectorIcon(MyIcons.Account), "https://github.com/damahecode/Jetpack-Compose-UI"),
-        FeaturedList(context.getString(R.string.login_signup), ImageVectorIcon(MyIcons.Account), "https://github.com/damahecode/Jetpack-Compose-UI"),
+        FeaturedList(context.getString(R.string.profile), ImageVectorIcon(MyIcons.Account), profileScreenNavRoute),
+        FeaturedList(context.getString(R.string.login_signup), ImageVectorIcon(MyIcons.Account), loginSignupScreenNavRoute),
+        FeaturedList(context.getString(R.string.chatScreen), ImageVectorIcon(MyIcons.Send), chatScreenNavRoute),
+        FeaturedList(context.getString(R.string.on_boarding), ImageVectorIcon(MyIcons.List), onBoardingScreenNavRoute),
+        FeaturedList(context.getString(R.string.empty_screen), ImageVectorIcon(MyIcons.Info), emptyScreenNavRoute),
+        FeaturedList(context.getString(R.string.settings), ImageVectorIcon(MyIcons.Settings), settingsScreenNavRoute),
+        FeaturedList(context.getString(R.string.pin_loc_biometric), ImageVectorIcon(MyIcons.Lock), locBiometricScreenNavRoute),
     )
 
     fun templateListItems(context: Context) = listOf(
-        FeaturedList(context.getString(R.string.responsive_screen), DCodeIcon.DrawableResourceIcon(MyIcons.CodeTag), "https://github.com/damahecode/Jetpack-Compose-UI"),
+        FeaturedList(context.getString(R.string.responsive_screen), DCodeIcon.DrawableResourceIcon(MyIcons.CodeTag), ""),
     )
 }
