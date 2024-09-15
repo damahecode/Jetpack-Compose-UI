fc61e06de1c223e6ebd38ae696efed471764fa81
Damahe Code <damahecode@gmail.com>
2024-09-15 14:58:44 +0530

    removed unused code

--- core/ui/src/main/java/com/code/damahe/ui/screen/main/TemplateScreen.kt
+++ core/ui/src/main/java/com/code/damahe/ui/screen/main/TemplateScreen.kt
@@ -1,55 +1,17 @@
 package com.code.damahe.ui.screen.main
 
 import android.content.Context
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContent
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.semantics.testTagsAsResourceId
 import com.code.damahe.demo.ui.model.FeaturedList
-import com.code.damahe.res.R
 import com.code.damahe.ui.utils.DemoDataProvider
 
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun TemplateScreen(onClick: (FeaturedList, Context) -> Unit) {
     val context = LocalContext.current
 
-    Scaffold(
-        modifier = Modifier.semantics {
-            testTagsAsResourceId = true
-        },
-        containerColor = Color.Transparent,
-        contentColor = MaterialTheme.colorScheme.onBackground,
-        contentWindowInsets = WindowInsets.safeContent,
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.template))
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = Color.Transparent,
-                ),
-            )
-        }
-    ) { padding ->
-
-        FeaturedListContent(
-            Modifier.padding(padding),
-            onClick,
-            DemoDataProvider.templateListItems(context)
-        )
-    }
+    FeaturedListContent(
+        onClick = onClick,
+        list = DemoDataProvider.templateListItems(context)
+    )
 }
